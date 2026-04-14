2f05715400fd4b3174439430b0f6502d8a663aec
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2026-04-14 23:51:19 +0200

    Add .env as environmental variable to store API key

--- src/js/api.js/api.js
+++ src/js/api.js/api.js
@@ -1,18 +1,18 @@
 const api = (userInput) => {
 
-  function getAPIData(flavour) {
+  function getAPIData(category) {
 
     let url;
-
+    
     const options = {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NTJhZmY5MWUyZDU3MjBiNmVkNTc0MTUxY2RhZWRmYSIsIm5iZiI6MTc3NjExNDgyMy44MjIsInN1YiI6IjY5ZGQ1Yzg3M2QwMjdkNWVhOGQ5M2I3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IdglMUZG8uVpADdT97TiddyhT2SLJ4qTO4sOMiiXzU0'
+        Authorization: `${import.meta.env.VITE_API_KEY}`,
       }
     };
 
-    switch (flavour) {
+    switch (category) {
       case 'movie':
         url = new URL('/api/search/movie', window.location.origin);
         url.searchParams.set('query', userInput);
