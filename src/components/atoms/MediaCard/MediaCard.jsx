bcac3d784b919ff4adbbc3b6c83e2a18fe742583
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2026-04-22 00:05:10 +0200

    Add MediaCard.jsx

--- src/components/atoms/MediaCard/MediaCard.jsx
+++ src/components/atoms/MediaCard/MediaCard.jsx
@@ -1,4 +1,5 @@
 import styles from './_MediaCard.module.scss';
+import '../../../sass/abstract/_utils.scss';
 
 import BookmarkItem from '../BookmarkItem/BookmarkItem.jsx';
 import movie from '../../../assets/category/category_movie.svg';
@@ -8,43 +9,42 @@ export default function MediaCard() {
         <>
             {/* {isTrending ?
                 (
-                    <article className={styles.mediacard_trending}>
-                        <div className={styles.mediacard_trending__container}>
-                            <BookmarkItem />
+                    <article className={`${styles.mediacard}  ${styles['mediacard--trending']}`}>
+                        <div className={styles.mediacard__container}>
+                            <BookmarkItem className={`${styles.mediacard__button}`} />
                             <div className={styles.mediacard__media_info}>
                                 <div className={`${styles.mediacard__media_date_and_type}  text_preset_5  text_white--opaque_75`}>
-                                    <span>2019</span>
-                                    <div className={styles.mediacard__media_category}>
+                                    <span className={`${styles.separator}  ${styles['separator--trending']}`}>2019</span>
+                                    <div className={`${styles.mediacard__media_category}  ${styles['separator']}  ${styles['separator--trending']}`}>
                                         <img src={movie} alt='' />
-                                        <span>Movie</span>
+                                        <span className='text_capitalize'>movie</span>
                                     </div>
                                     <span className='text_uppercase'>pg</span>
                                 </div>
-                                <h3 className={`${styles.mediacard__media_title}  text_preset_3  text_white  text_capitalize`}>beyond earth</h3>
+                                <h3 className={`text_preset_3   text_white   text_capitalize`}>beyond earth</h3>
                             </div>
                         </div>
                     </article>
                 ) : (
-                    <article className={styles.mediacard}></article>
+                    <article className={styles.mediacard}>
+                        <div className={`${styles.mediacard__container}`}>
+                            <BookmarkItem className={`${styles.mediacard__button}  ${styles['mediacard__button--untrending']}`} />
+                        </div>
+
+                        <div className={`${styles.mediacard__media_info}  ${styles.mediacard__text_outside}`}>
+                            <div className={`${styles.mediacard__media_date_and_type}  text_preset_6  text_white--opaque_75`}>
+                                <span className={styles.separator}>2019</span>
+                                <div className={`${styles.mediacard__media_category}  ${styles['separator']}`}>
+                                    <img src={movie} alt='' />
+                                    <span className='text_capitalize'>movie</span>
+                                </div>
+                                <span className={`${styles.separator}  text_uppercase`}>pg</span>
+                            </div>
+                            <h3 className='text_uppercase'>beyond earth</h3>
+                        </div>
+                    </article>
                 )
             } */}
-
-            <article className={styles.mediacard_trending}>
-                <div className={styles.mediacard_trending__container}>
-                    <BookmarkItem />
-                    <div className={styles.mediacard__media_info}>
-                        <div className={`${styles.mediacard__media_date_and_type}  text_preset_5  text_white--opaque_75`}>
-                            <span>2019</span>
-                            <div className={styles.mediacard__media_category}>
-                                <img src={movie} alt='' />
-                                <span>Movie</span>
-                            </div>
-                            <span className='text_uppercase'>pg</span>
-                        </div>
-                        <h3 className={`${styles.mediacard__media_title}  text_preset_3  text_white  text_capitalize`}>beyond earth</h3>
-                    </div>
-                </div>
-            </article>
         </>
     )
 }
